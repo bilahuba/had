57dd6e4af182d1cd5e2f119edf696aa18fa81c49
lmichalik15 <119747293+lmichalik15@users.noreply.github.com>
2022-12-03 18:20:14 +0100

    Add files via upload

--- Had.cpp
+++ Had.cpp
@@ -6,24 +6,20 @@
 
 using namespace std;
 
-#define A 40 // sirka pole
-#define B 20 // vyska pole
+#define widthField 40 // sirka pole
+#define heightField 20 // vyska pole
 
 int i;
 int j;
+int pole[widthField][heightField];
 
-
-
-
-
-void main()
-{
-	for ( i=0; i <= A+1; i++)
+void printField() {                            // funkce pro vykreslení čtverce pole
+	for (i = 0; i <= widthField + 1; i++)
 	{
 		if (i == 0) {
-			printf("%c", 201);
+			printf("%c", 201);                 // %c - znaky asci tabulka  
 		}
-		else if (i == A+1) {
+		else if (i == widthField + 1) {
 			printf("%C", 187);
 		}
 		else
@@ -33,13 +29,13 @@ void main()
 	}
 	printf("\n");
 
-	for (i = 0; i < B; i++)
+	for (i = 0; i < heightField; i++)
 	{
 		printf("%c", 186);
-		for (j = 0; j < A; j++) {
-			if (j==A-1)
+		for (j = 0; j < widthField; j++) {
+			if (j == widthField - 1)
 			{
-					printf(" %c\n", 186);
+				printf(" %c\n", 186);
 			}
 			else
 			{
@@ -48,12 +44,12 @@ void main()
 		}
 	}
 
-	for (i = 0; i <= A + 1; i++)
+	for (i = 0; i <= widthField + 1; i++)
 	{
 		if (i == 0) {
 			printf("%c", 200);
 		}
-		else if (i == A + 1) {
+		else if (i == widthField + 1) {
 			printf("%C", 188);
 		}
 		else
@@ -61,6 +57,23 @@ void main()
 			printf("%c", 205);
 		}
 	}
+}
+
+
+
+void snakeInitialization() {                       // funkce vykreslení hada
+
+
+
+}
+
+
+
+
+
+void main(){
+	printField();
+	snakeInitialization();
 
 }
 
