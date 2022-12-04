fee8ea3e6bd8f741a1c5e6c32ed2188a3de619ef
lmichalik15 <119747293+lmichalik15@users.noreply.github.com>
2022-12-04 01:45:06 +0100

    update 1.45

--- Had.cpp
+++ Had.cpp
@@ -6,7 +6,7 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <conio.h>
-#include <ctime>
+#include <time.h>
 
 using namespace std;
 
@@ -23,8 +23,10 @@ int head;
 int tail;
 int game;
 int fruit;
-int randomA;
-int randomB;
+int A;
+int B;
+int var;
+int direction;
 
 
 
@@ -37,13 +39,14 @@ void snakeInitialization() {                       // funkce vykreslení hada
 		}
 	}
 
-	x = (heightField / 2);                           // vychozi pozice hada
-	y = (widthField / 2);
+	x = (heightField / 2) - 1;                           // vychozi pozice hada
+	y = (widthField / 2) - 1;
 	Gy = y;
 	head = 5;
 	tail = 1;
 	game = 0;
 	fruit = 0;
+	direction = 77;
 
 
 	for (i = 0; i < head; i++)                    // loop kde se kontroluje delka hada, moc nechapu!!
@@ -120,13 +123,14 @@ void printField() {                            // funkce pro vykreslení čtverc
 
 void randomFruit() {                                   // nahodna pozice ovoce
 	srand(time(0));                      
-	randomA = 1 + rand() % 18;
-	randomB = 1 + rand() % 28;
+	A = 1 + rand() % 18;
+	B = 1 + rand() % 38;
 
+	
 
-	if (fruit == 0 && field[randomA][randomB] == 0)
+	if (fruit == 0 && field[A][B] == 0)
 	{
-		field[randomA][randomB] = -1;
+		field[A][B] = -1;
 		fruit = 1;
 	}
 	
@@ -144,8 +148,83 @@ void ResetScreenPosition()                            // zakotvení obrazu při 
 	SetConsoleCursorPosition(hOut, Position);
 }
 
+int getStart()
+{
+	if (_kbhit())
+	
+		return _getch();
+	
+	else
+	
+		return -1;
+	
+}
 
-void main()
+void movement() {                            // pohyb
+	var = getStart();
+	/*
+	if ((var == 77 || var == 75 || var == 72 || var == 80) && abs(direction - var)>2)
+	{
+		direction = var;
+	} */
+	
+	if ((var == 77 || var == 75) && abs(direction - var) > 2)
+	{
+		direction = var;
+	}
+	
+	 if ((var == 72 || var == 80) && abs(direction - var) > 8)
+	{
+		direction == var;
+	}
+	
+
+
+
+	if (direction == 77) {                   // doprava
+		y++;
+		head++;
+		field[x][y] = head;
+	}
+
+	if (direction == 75) {                   // doleva
+		y--;
+		head++;
+		field[x][y] = head;
+	}
+
+	if (direction == 72) {                   // nahoru
+		x--;
+		head++;
+		field[x][y] = head;
+	}
+
+	if (direction == 80) {                   // dolu
+		x++;
+		head++;
+		field[x][y] = head;
+	}
+}
+
+
+void tailRemove()                   // odebrani konce hada
+{
+	for ( i = 0; i < heightField; i++)
+	{
+		for ( j = 0; j < widthField; j++)
+		{
+			if (field[i][j] == tail)
+			{
+				field[i][j] == 0;
+			}
+		}
+	}
+	tail++;
+}
+
+
+
+int main()
 {
 	snakeInitialization();
 
@@ -154,7 +233,9 @@ void main()
 		printField();
 		ResetScreenPosition();
 		randomFruit();
-
+		movement();
+		Sleep(99);
+		tailRemove;
 	}
 	
 }
